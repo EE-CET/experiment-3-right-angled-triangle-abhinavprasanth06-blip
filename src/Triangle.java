2fc979fb14e35ab63ed016d741e34f780a09a288
Your NameAbhinav <abhinavprasanth06@gmail.com>
2026-01-30 10:43:47 +0530

    added triangle

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,3 +1,12 @@
-public class Triangle {
-    //Add your programs here
+public class Triangle{
+	public static void main(String[] args){
+		for(int i=0;i<3;i++){
+			//int j =1;
+			for(int j=0;j<2*i+1;j++){
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}
+	}
 }
